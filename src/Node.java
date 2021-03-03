85a98b89738d3134a0b862959bad67e562893d4a
Abrar051 <shahriar1410051@gmail.com>
2021-03-03 12:42:02 +0600

    LinkedList

---
+++ src/Node.java
@@ -0,0 +1,11 @@
+
+class Node {
+    int data;
+    Node next;
+    Node previous;
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+        //previous = null;
+    }
+}
