ec823b45d4c0c752919d0c5a0a2af99339f9baca
Abrar051 <shahriar1410051@gmail.com>
2021-03-14 11:49:21 +0600

    LinkedList

--- src/ArithmeticEvaluation/EvaluateString.java
+++ src/ArithmeticEvaluation/EvaluateString.java
@@ -2,13 +2,14 @@ package ArithmeticEvaluation;
 
 import java.util.Stack;
 
+
 public class EvaluateString {
 
 
     public static int evaluate(String str) {
         char[] ch = str.toCharArray();
         Stack<Integer> values = new Stack<Integer>();
-        Stack<Character> ops = new Stack<Character>();
+        Stack<Character> operator = new Stack<Character>();
         for (int i = 0; i < ch.length; i++) {
             if (ch[i] == ' ')
                 continue;
@@ -18,24 +19,25 @@ public class EvaluateString {
                     buff.append(ch[i++]);
                 values.push(Integer.parseInt(buff.toString()));
                 i--;
-            } else if (ch[i] == '(')
-                ops.push(ch[i]);
+            }
+            else if (ch[i] == '(')
+                operator.push(ch[i]);
             else if (ch[i] == ')') {
-                while (ops.peek() != '(')
-                    values.push(operation(ops.pop(), values.pop(), values.pop()));
-                ops.pop();
+                while (operator.peek() != '(')
+                    values.push(operation(operator.pop(), values.pop(), values.pop()));
+                operator.pop();
             } else if (ch[i] == '+' || ch[i] == '-' || ch[i] == '*' || ch[i] == '/') {
-                while (!ops.empty() && presedence(ch[i], ops.peek()))
-                    values.push(operation(ops.pop(), values.pop(), values.pop()));
-                ops.push(ch[i]);
+                while (!operator.empty() && signOperation(ch[i], operator.peek()))
+                    values.push(operation(operator.pop(), values.pop(), values.pop()));
+                operator.push(ch[i]);
             }
         }
-        while (!ops.empty())
-            values.push(operation(ops.pop(), values.pop(), values.pop()));
+        while (!operator.empty())
+            values.push(operation(operator.pop(), values.pop(), values.pop()));
         return values.pop();
     }
 
-    public static boolean presedence(char op1, char op2) {
+    public static boolean signOperation (char op1, char op2) {
         if (op2 == '(' || op2 == ')')
             return false;
         if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
